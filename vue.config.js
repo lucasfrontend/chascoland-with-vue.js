58337d4658f46ce905d467ffcb686d425cb44557
lucasfrontend <lucasmartinezguardavidas@gmail.com>
2022-11-22 19:38:41 -0300

    transpileDep

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" 
-  ? "/chascoland-with-vue.js" 
-  : "/",
+  publicPath: "/chascoland-with-vue.js",
   transpileDependencies: true,
 })
